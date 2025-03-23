ac00bc99c9d721d570937d52b762c5b36b2296a0
Eduardo <edu.frcarv@gmail.com>
2025-03-23 23:56:27 +0000

    feito caralho

--- src/Footer.js
+++ src/Footer.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Footer(){
-    return(
-        <div class="footer_contacts">
+function Footer() {
+    const [showNames, setShowNames] = useState(false);
+
+    const toggleNames = () => {
+        setShowNames(!showNames);
+    };
+
+    return (
+        <div className="footer_contacts">
             <section>
-        <h2>Contactos</h2>
-      </section>
+                <h2>Contactos</h2>
+            </section>
 
-      <footer>
-        <a id="email" href="mailto:vilarMourosSupport@gmail.com">vilarMourosSupport@gmail.com</a>  
-      </footer>
-      <script src="script.js"></script>
+            <footer onClick={toggleNames} style={{ cursor: 'pointer' }}>
+                <a id="email" href="mailto:vilarMourosSupport@gmail.com">vilarMourosSupport@gmail.com</a>
+                {showNames && (
+                    <div className="names">
+                        <p>Eduardo Carvalho</p>
+                        <p>Rodrigo Leite</p>
+                        <p>Joao Soares</p>
+                    </div>
+                )}
+            </footer>
+            <script src="script.js"></script>
         </div>
     );
-
 }
 
 export default Footer;
