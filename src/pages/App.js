703f6c12d8941dae75aed5b27fc9c00b7c84a57f
Rleite21 <rrressleite@gmail.com>
2025-03-30 23:49:35 +0100

    Create App.js

---
+++ src/pages/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from '../Header';
+import SlideShowContainer from '../SlideShowContainer';
+import ArtistPage from '../ArtistPage';
+import PrevisaoClimaterica from '../PrevisaoClimaterica';
+import Footer from '../Footer';
+import table from '../table.json';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <SlideShowContainer />
+      <ArtistPage artists={table.artists}/>
+      <PrevisaoClimaterica previsoes={table.previsoes}/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
